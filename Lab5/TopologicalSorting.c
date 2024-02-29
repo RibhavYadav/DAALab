ef09fbdb93e9b0a5f1c818fc74f86e4b6f150d06
draby2466 <ribhavyadav2466@gmail.com>
2024-03-01 00:34:27 +0530

    Lab 5: Updated topological sorting and added diameter of BST

--- Lab5/TopologicalSorting.c
+++ Lab5/TopologicalSorting.c
@@ -1,39 +1,34 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-#define size 6
-int stack[size], visited[size], top = 0;
-int adjMat[size][size] = {
+#define size 5
+int stack[size], visited[size], top = -1;
+int adjMatrix[size][size] = {
+        {0, 1, 0, 0, 1},
+        {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {1, 1, 1, 1, 0}
+        {0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 0}
 };
 
 void topologicalSort(int node) {
     visited[node] = 1;
-    for (int i = 0; i < size - 1; i++) {
-        if (visited[i] == 0 && adjMat[node][i] == 1) {
+    for (int i = 0; i < size; i++) {
+        if (adjMatrix[node][i] && !visited[i]) {
             topologicalSort(i);
-
         }
     }
     stack[++top] = node;
 }
 
-
 void main() {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
         visited[i] = 0;
-    }
     for (int i = 0; i < size; i++) {
-        if (visited[i] == 0) {
+        if (!visited[i]) {
             topologicalSort(i);
         }
     }
-    for (int i = size; i >= 1; i--) {
-        printf("%d->", stack[i]);
-    }
+    printf("Topological Order is: \n");
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
 }

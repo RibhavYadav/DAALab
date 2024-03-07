d8b4868336491d54a3fe17fe2c4dc68cb5f405b2
draby2466 <ribhavyadav2466@gmail.com>
2024-03-07 21:23:23 +0530

    Updated consecutive integer checking method for GCD.

--- Lab2/GCD.c
+++ Lab2/GCD.c
@@ -20,18 +20,14 @@ int GCD(int n, int m) {
 }
 
 int main() {
-    FILE *fp = fopen("GCD.csv", "w+");
-    fprintf(fp, "m + n, Basic operation, Time\n");
     clock_t start, end;
     for (int i = 0; i < 100; i++) {
-        int m = random(), n = rand(), ops;
+        int m = rand(), n = rand(), ops;
         start = clock();
         ops = GCD(n, m);
         end = clock();
         double time = (double) (end - start) / CLOCKS_PER_SEC;
         printf("%d, %d, %f\n", (m + n), ops, time);
-        fprintf(fp, "%d, %d, %f\n", (m + n), ops, time);
     }
-    fclose(fp);
     return 0;
 }

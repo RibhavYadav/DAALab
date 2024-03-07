d79e9f24e244471a4a57bea9f6a0b6823dbe51eb
draby2466 <ribhavyadav2466@gmail.com>
2024-03-07 23:40:37 +0530

    Updated adjacency list

--- Lab1/AdjacencyList.c
+++ Lab1/AdjacencyList.c
@@ -33,8 +33,33 @@ graph createGraph(int vertices) {
 void addEdge(graph gp, int n1, int n2) {
     node node1 = getNode(n1);
     node node2 = getNode(n2);
-    gp->adjList[n1] = node1;
-    gp->adjList[n2] = node2;
     node1->next = gp->adjList[n2];
     node2->next = gp->adjList[n1];
+    gp->adjList[n1] = node2;
+    gp->adjList[n2] = node1;
+}
+
+void printGraph(graph gp) {
+    for (int i = 0; i < gp->vertices; i++) {
+        node temp = gp->adjList[i];
+        printf("%d: ", i);
+        while (temp != NULL) {
+            printf("%d->", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+void main() {
+    graph gp = createGraph(5);
+    for (int i = 0; i < 5; i++) {
+        int n1, n2;
+        printf("Enter node: ");
+        scanf("%d", &n1);
+        printf("Enter node: ");
+        scanf("%d", &n2);
+        addEdge(gp, n1, n2);
+    }
+    printGraph(gp);
 }
